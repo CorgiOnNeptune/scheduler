158435393556f030d86103d62a2597ea120f179a
Justin <justinjdbennett@gmail.com>
2023-01-10 17:11:29 -0700

    Move all .scss files to styles folder

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,7 @@
 import classNames from 'classnames';
 import React from 'react';
 
-import 'components/Button.scss';
+import 'styles/Button.scss';
 
 export default function Button(props) {
   const buttonClass = classNames('button', {
--- stories/index.js
+++ stories/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { action } from '@storybook/addon-actions';
 import { storiesOf } from '@storybook/react';
 
-import 'index.scss';
+import 'styles/index.scss';
 
 import Button from 'components/Button';
 

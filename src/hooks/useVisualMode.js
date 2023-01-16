cdfcb5a62c4f8878ef6fc4dfd6046e3ec2f0a228
Justin <justinjdbennett@gmail.com>
2023-01-15 19:48:49 -0700

    Create useVisualMode and tests

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,7 +1,27 @@
 import { useEffect, useState } from 'react';
 
-export default function useVisualMode(mode) {
-  const [current, setCurrent] = useState({ mode: 'FIRST' });
+export default function useVisualMode(initial) {
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
 
-  return { mode };
+  const transition = (newMode, replace = false) => {
+    if (replace) {
+      history.pop();
+    }
+
+    setMode(newMode);
+    setHistory([...history, newMode]);
+  };
+
+  const back = () => {
+    if (history.length <= 1) return;
+
+    history.pop();
+    setMode(history[history.length - 1]);
+  };
+
+  console.log('history ➡️ ', `${history}`);
+  console.log('mode ➡️ ', `${mode}`);
+
+  return { mode, transition, back };
 }

2459a1f39e2d68d3dfcdeb030791e5f576bd516c
Justin <justinjdbennett@gmail.com>
2023-01-16 17:24:35 -0700

    Implement api PUT to appointments and status transition

--- src/components/Application.jsx
+++ src/components/Application.jsx
@@ -32,7 +32,10 @@ export default function Application(props) {
       [id]: appointment,
     };
 
-    setState({ ...state, appointments });
+    return axios
+      .put(`http://localhost:8001/api/appointments/${id}`, appointments[id])
+      .then(setState({ ...state, appointments }))
+      .catch((err) => err.message);
   };
 
   // Get days data from API and set the days state
@@ -41,20 +44,22 @@ export default function Application(props) {
       axios.get('http://localhost:8001/api/days'),
       axios.get('http://localhost:8001/api/appointments'),
       axios.get('http://localhost:8001/api/interviewers'),
-    ]).then((all) => {
-      console.log('↓ /api/days ↓');
-      console.log(all[0].data);
-      console.log('↓ /api/appointments ↓');
-      console.log(all[1].data);
-      console.log('↓ /api/interviewers ↓');
-      console.log(all[2].data);
-      setState((prev) => ({
-        ...prev,
-        days: all[0].data,
-        appointments: all[1].data,
-        interviewers: all[2].data,
-      }));
-    });
+    ])
+      .then((all) => {
+        console.log('↓ /api/days ↓');
+        console.log(all[0].data);
+        console.log('↓ /api/appointments ↓');
+        console.log(all[1].data);
+        console.log('↓ /api/interviewers ↓');
+        console.log(all[2].data);
+        setState((prev) => ({
+          ...prev,
+          days: all[0].data,
+          appointments: all[1].data,
+          interviewers: all[2].data,
+        }));
+      })
+      .catch((err) => err.message);
   }, []);
 
   const dailyAppointments = getAppointmentsForDay(state, state.day);
--- src/components/Appointment/index.jsx
+++ src/components/Appointment/index.jsx
@@ -6,10 +6,12 @@ import Header from './Header';
 import Show from './Show';
 import Empty from './Empty';
 import Form from './Form';
+import Status from './Status';
 
 const EMPTY = 'EMPTY';
 const SHOW = 'SHOW';
 const CREATE = 'CREATE';
+const SAVING = 'SAVING';
 
 export default function Appointment(props) {
   const { mode, transition, back } = useVisualMode(
@@ -17,19 +19,25 @@ export default function Appointment(props) {
   );
 
   const save = (name, interviewer) => {
+    transition(SAVING);
+
     const interview = {
       student: name,
       interviewer,
     };
 
-    props.bookInterview(props.id, interview);
-    transition(SHOW);
+    // Putting timeout here just to show saving animation because otherwise it is way too quick and it fully skips the transition
+    setTimeout(
+      () => props.bookInterview(props.id, interview) && transition(SHOW),
+      1000
+    );
   };
 
   return (
     <article className="appointment">
       <Header time={props.time} />
       {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === SAVING && <Status message="Saving" />}
       {mode === SHOW && (
         <Show
           student={props.interview.student}
